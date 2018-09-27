ae7fdd2c6995233455bfe111b9941a29a8eea1e3
hoc-anms <phamvanhoc96@gmail.com>
2018-09-27 11:34:59 +0700

    add function to create crud for author

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use App\Categories;
+use App\Author;
 class BookController extends Controller
 {
     //
-    public function getAdd(){
 
-    }
     public function getEdit(){
 
     }
     public function getList(){
+        $cate=Categories::all();
+        $author=Author::all();
         $book=DB::select('SELECT b.b_id,b.b_name,c.c_name,a.a_name,b.created_at,b.b_status FROM book b,author a,categories c WHERE b.b_aid = a.a_id AND b.b_cid = c.c_id');
-        return view('admin.book.list',['book'=>$book]);
+        return view('admin.book.list',compact('book','author','cate'));
     }
 }

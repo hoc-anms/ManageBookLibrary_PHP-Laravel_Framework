c8c0613a67ca2425cc02e5c42d6e7421eb5698e6
hoc-anms <phamvanhoc96@gmail.com>
2018-09-27 11:32:02 +0700

    Crud in function author

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,18 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Author;
+use Validator;
+use Response;
+use Illuminate\Support\Facades\Input;
+use App\http\Requests;
 
 class AuthorController extends Controller
 {
-    //
-    public function getAdd(){
 
-    }
-    public function getEdit(){
-
-    }
     public function getList(){
         $author=Author::all();
         return view('admin.author.list',['author'=>$author]);
     }
+
+    public function addAuthor(Request $request){
+        $author= new Author();
+        $author->a_name=$request->a_name;
+        $author->save();
+        return response()->json($author);
+    }
+
+    public function editAuthor(Request $request){
+
+        $author = Author::find($request->a_id);
+        $author->a_name = $request->a_name;
+        $author->save();
+        return response()->json($author);
+    }
+    public function deleteAuthor(Request $request){
+        $author=Author::find($request->a_id)->delete();
+        return response()->json();
+    }
+
+
 }

9ec6fd8dc3f064255c8141c6e4bcf222cdf3b4a3
hoc-anms <phamvanhoc96@gmail.com>
2018-09-27 11:39:31 +0700

    model Categories

--- app/Categories.php
+++ app/Categories.php
@@ -8,7 +8,8 @@ class Categories extends Model
 {
     //
     protected $table="categories";
-
+    protected $fillable=['c_name','c_parent'];
+    protected $primaryKey='c_id';
     public function bookCat(){
         return $this->hasMany('App\Book','b_cid','c_id');
     }

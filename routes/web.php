4a03e42f0783e351c1c6c5f2e202e48ccdfd37f9
hoc-anms <phamvanhoc96@gmail.com>
2018-10-05 17:51:43 +0700

    update final route

--- routes/web.php
+++ routes/web.php
@@ -7,57 +7,51 @@ Route::get('/', function () {
 });
 
 
-Route::get('thu',function (){
-
-    $categories=Categories::where('c_id',1)->first();
-    foreach ($categories->book as $book){
-        echo $book->b_name;
-        echo '<br>';
-    }
-});
-
 Route::get('header',function (){
    return view('admin.layouts.header');
 });
 
-
+//Route to index dashboard
 Route::get('index',function (){
     return view('admin.layouts.index');
 });
+//Route User
+Route::get('admin/login','UserController@getLoginAdmin');
+Route::post('admin/login','UserController@postLoginAdmin');
 
-Route::get('login',function (){
-    return view ('admin.login');
-});
+Route::get('admin/logout','UserController@getLogoutAdmin');
 
-//Route::group(['middleware'=>['web']],function (){
-//    Route::resource('index','CategoriesController');
-//    Route::POST('addPost','CategoriesController@addPost');
-//});
+Route::get('admin/register','UserController@getRegisterAdmin');
+Route::post('admin/register','UserController@postRegisterAdmin');
 
+//Route manage book
+Route::group(['prefix'=>'admin','middleware'=>'adminLogin'],function (){
 
+//Route to index dashboard
+    Route::get('index',function (){
+        return view('admin.layouts.index');
+    });
 
-
-
-
-Route::group(['prefix'=>'admin'],function (){
     Route::group(['prefix' => 'categories'], function () {
 //       admin/categories/add..
-
-
-        Route::get('edit', 'CategoriesController@getEdit');
-
+        Route::POST('addCategories','CategoriesController@addCategories');
+        Route::POST('editCategories','CategoriesController@editCategories');
+        Route::POST('deleteCategories','CategoriesController@deleteCategories');
         Route::get('list', 'CategoriesController@getList');
     });
     Route::group(['prefix' => 'author'], function () {
 //       admin/author/add..
-
+        Route::POST('addAuthor','AuthorController@addAuthor');
+        Route::POST('editAuthor','AuthorController@editAuthor');
+        Route::POST('deleteAuthor','AuthorController@deleteAuthor');
         Route::get('list', 'AuthorController@getList');
     });
     Route::group(['prefix' => 'book'], function () {
 //       admin/book/add..
 
-        Route::get('edit', 'BookController@getEdit');
-
+        Route::POST('addBook','BookController@addBook');
+        Route::POST('editBook','BookController@editBook');
+        Route::POST('deleteBook','BookController@deleteBook');
         Route::get('list', 'BookController@getList');
     });
 });

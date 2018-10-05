0c6c39da9803a8a1fa970256bc22ed58341cbd4c
hoc-anms <phamvanhoc96@gmail.com>
2018-10-05 17:30:07 +0700

    update header for index

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -13,9 +13,10 @@
     <title>Management Categories Book Library</title>
     <base href="{{asset('')}}">
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
             integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
             crossorigin="anonymous"></script>
@@ -27,9 +28,11 @@
     <link rel="stylesheet" href="css/style.css">
 
 
-    <link href="bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
     <link href="bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
+    <link href="bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
+
     <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
 
 </head>
 <body>
@@ -43,31 +46,37 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href=""><i class="fa fa-home">&nbsp;</i>Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="index"><i class="fa fa-home">&nbsp;</i>Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href=""><i class="fa fa-user">&nbsp;</i>Logout</a>
+                        <a class="nav-link" href="admin/logout"><i class="fa fa-user">&nbsp;</i>Logout</a>
                     </li>
                 </ul>
             </div>
         </nav>
     </div>
     <br><br><br>
-    @yield('content');
+    <div> @yield('content')</div>
 
 
-<!-- DataTables JavaScript -->
-<script src="bower_components/DataTables/media/js/jquery.dataTables.min.js"></script>
 
-<!-- Page-Level Demo Scripts - Tables - Use for reference -->
-<script>
-    $(document).ready(function() {
-        $('#dataTables-example').DataTable({
-            responsive: true
+
+<div>
+
+    <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
+    <!-- DataTables JavaScript -->
+    <script src="bower_components/DataTables/media/js/jquery.dataTables.min.js"></script>
+    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+    <script>
+        $(document).ready(function() {
+            $('#dataTables-example').DataTable({
+                responsive: true
+            });
         });
-    });
+    </script>
+    {{----}}
 
-</script>
+</div>
 </body>
 </html>
 
